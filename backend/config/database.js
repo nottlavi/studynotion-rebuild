37581d7239f2debc686e9ce70eca01333b056ffe
Lavi <laviarora4234@gmail.com>
2025-11-16 19:43:35 +0530

    persist user across browser sessions

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,7 +5,6 @@ exports.connect = () => {
     .connect(process.env.MONGODB_URL)
     .then((connection) => {
       console.log("db connected successfully");
-      console.log(connection.connection.host);
     })
     .catch((err) => {
       console.log("connection to be couldnt be established"),
