9952e6690948dfd55acc308110ac2a7ec6b86fd3
Lavi <arora.lavi.dev@gmail.com>
2025-11-20 13:48:36 +0530

    add frontend to view profile details

--- frontend/src/pages/dashboard/ProfilePage.jsx
+++ frontend/src/pages/dashboard/ProfilePage.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
 
 export const ProfilePage = () => {
-  return (
-    <div>ProfilePage</div>
-  )
-}
+  //redux stuff here
+  const token = useSelector((state) => state.user.token);
+
+  //managing states here
+  const [profile, setProfile] = useState({});
+
+  //managing dependencies here
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+  useEffect(() => {
+    const getProfileFromBackend = async () => {
+      try {
+        const res = await axios.get(`${BASE_URL}/users/get-profile`, {
+          withCredentials: true,
+        });
+
+        if (res) setProfile(res.data.user);
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+    getProfileFromBackend();
+  }, [token]);
+
+  return <div>
+    {profile.firstName}
+    {profile.lastName}
+    <br />
+    {profile.email}
+  </div>;
+};
