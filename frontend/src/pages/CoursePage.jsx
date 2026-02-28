f3714de022540bcfe96a73c67e7d4ff4941a8b8d
nottlavi <arora.lavi.dev@gmail.com>
2026-02-28 09:55:33 +0530

    add redux thunk for fetching updated profile

--- frontend/src/pages/CoursePage.jsx
+++ frontend/src/pages/CoursePage.jsx
@@ -42,7 +42,6 @@ export const CoursePage = () => {
         { withCredentials: true },
       );
       if (res) {
-        console.log(res);
         dispatch(addToCart());
         dispatch(increaseTotal(currentCourse.price));
       }
--- frontend/src/slices/userSlice.js
+++ frontend/src/slices/userSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 //defining the initial state
 const initialState = {
@@ -6,6 +7,25 @@ const initialState = {
   token: "",
   profile: {},
 };
+
+export const fetchUserProfile = createAsyncThunk(
+  "user/fetchUserProfile",
+  async (_, { rejectWithValue }) => {
+    try {
+      const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+      const res = await axios.get(`${BASE_URL}/users/get-profile`, {
+        withCredentials: true,
+      });
+
+      return res.data.user;
+    } catch (err) {
+      return rejectWithValue(
+        err.response?.data || "Failed to fetch user profile",
+      );
+    }
+  },
+);
 
 const userSlice = createSlice({
   name: "user",
@@ -29,6 +49,11 @@ const userSlice = createSlice({
     clearProfile: (state) => {
       state.profile = {};
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchUserProfile.fulfilled, (state, action) => {
+      state.profile = action.payload;
+    });
   },
 });
 
