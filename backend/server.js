cd7a4fe752d093e1245cb34ac838123ff2d47b9d
nottlavi <arora.lavi.dev@gmail.com>
2026-05-17 16:22:53 +0530

    fixes:(

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "https://studynotion-rebuild.vercel.app/",
-    ],
+    origin: ["http://localhost:3000", "https://studynotion-rebuild.vercel.app"],
     credentials: true,
   }),
 );
