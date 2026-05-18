50014069df9bd6e2ac77e083e2a2a59b18768fcc
nottlavi <arora.lavi.dev@gmail.com>
2026-05-18 17:08:42 +0530

    switch from smtp to http

--- backend/utils/mailSender.js
+++ backend/utils/mailSender.js
@@ -1,29 +1,49 @@
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
-
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-});
+const axios = require("axios");
 
 exports.sendMail = async (email, subject, text, html) => {
   try {
-    const info = await transporter.sendMail({
-      from: process.env.MAIL_SENDER,
-      to: email,
-      subject: subject,
-      text: String(text),
-      html: html || undefined,
-    });
+    const response = await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: {
+          email: process.env.MAIL_SENDER,
+        },
+
+        to: [
+          {
+            email: email,
+          },
+        ],
+
+        subject: subject,
+
+        textContent: String(text),
+
+        htmlContent: html || undefined,
+      },
+
+      {
+        headers: {
+          accept: "application/json",
+          "api-key": process.env.BREVO_API_KEY,
+          "content-type": "application/json",
+        },
+      },
+    );
+
+    console.log("MAIL SENT SUCCESSFULLY");
+    console.log(response.data);
 
     return true;
   } catch (err) {
     console.log("MAIL ERROR");
-    console.log(err);
+
+    if (err.response) {
+      console.log(err.response.data);
+    } else {
+      console.log(err);
+    }
+
+    return false;
   }
 };
